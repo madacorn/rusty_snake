c0d64760f6ccc7aa6272b6bb16483e36072ab80e
AStain <codingstain@gmail.com>
2024-02-03 08:47:09 +0100

    Adding a menu plugin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use bevy::prelude::*;
 
+mod menu;
+
+#[derive(PartialEq, Eq, Debug, Hash, Clone, Default, States)]
+pub enum GameState {
+    #[default]
+    Menu,
+    InGame,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins((DefaultPlugins, menu::MenuPlugin))
+        .add_state::<GameState>()
+        .add_systems(Startup, setup)
+        .run();
 }
