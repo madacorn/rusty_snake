4fad3e6b1493b0a0299d7131eba8e903cf961df9
AStain <codingstain@gmail.com>
2024-02-06 06:47:41 +0100

    Add game looping checks

--- src/game.rs
+++ src/game.rs
@@ -4,9 +4,28 @@ use bevy::prelude::*;
 fn load_game(mut _commands: Commands) {
     println!("Loading game");
 }
+
+fn handle_input(mut _commands: Commands) {
+    println!("Handle Input");
+}
+fn move_snake(mut _commands: Commands) {
+    println!("Move Snake");
+}
+fn detect_collision(mut _commands: Commands) {
+    println!("Detect Collision");
+}
+fn spawn_fruits(mut _commands: Commands) {
+    println!("Spawn fruits");
+}
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::InGame), load_game);
+        app.add_systems(OnEnter(GameState::InGame), load_game)
+            .add_systems(
+                Update,
+                (handle_input, move_snake, detect_collision, spawn_fruits)
+                    .chain()
+                    .run_if(in_state(GameState::InGame)),
+            );
     }
 }
