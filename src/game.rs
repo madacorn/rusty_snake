cfbd6bb5e626c4e0a843051d8fe07769df61361c
AStain <codingstain@gmail.com>
2024-02-06 07:18:20 +0100

    Adding handling input

--- src/game.rs
+++ src/game.rs
@@ -1,21 +1,53 @@
 use crate::GameState;
 use bevy::prelude::*;
 
+#[derive(Resource, Default)]
+enum SnakeDirection {
+    #[default]
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
 fn load_game(mut _commands: Commands) {
     println!("Loading game");
 }
 
-fn handle_input(mut _commands: Commands) {
-    println!("Handle Input");
+fn handle_input(keys: Res<Input<KeyCode>>, mut direccion: ResMut<SnakeDirection>) {
+    if keys.just_pressed(KeyCode::W) {
+        match *direccion {
+            SnakeDirection::Left | SnakeDirection::Right => *direccion = SnakeDirection::Up,
+            _ => (),
+        }
+    }
+    if keys.just_pressed(KeyCode::A) {
+        match *direccion {
+            SnakeDirection::Up | SnakeDirection::Down => *direccion = SnakeDirection::Left,
+            _ => (),
+        }
+    }
+    if keys.just_pressed(KeyCode::S) {
+        match *direccion {
+            SnakeDirection::Left | SnakeDirection::Right => *direccion = SnakeDirection::Down,
+            _ => (),
+        }
+    }
+    if keys.just_pressed(KeyCode::D) {
+        match *direccion {
+            SnakeDirection::Up | SnakeDirection::Down => *direccion = SnakeDirection::Right,
+            _ => (),
+        }
+    }
 }
-fn move_snake(mut _commands: Commands) {
-    println!("Move Snake");
+fn move_snake(_time: Res<Time>) {
+    //println!("Move Snake");
 }
 fn detect_collision(mut _commands: Commands) {
-    println!("Detect Collision");
+    //println!("Detect Collision");
 }
 fn spawn_fruits(mut _commands: Commands) {
-    println!("Spawn fruits");
+    //println!("Spawn fruits");
 }
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
@@ -26,6 +58,7 @@ impl Plugin for GamePlugin {
                 (handle_input, move_snake, detect_collision, spawn_fruits)
                     .chain()
                     .run_if(in_state(GameState::InGame)),
-            );
+            )
+            .init_resource::<SnakeDirection>();
     }
 }
